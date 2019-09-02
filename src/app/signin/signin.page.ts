84336c20ffb3a63fdeef2e566218869e0f0848e6
aqib feroz <aqibpakii@hotmail.com>
2019-09-03 03:15:36 +0500

    aqib

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  login(){
+this.router.navigate(['games'])
+  }
 }
+
