2b5fa855d741efe01fed9e41b185a7f0405f1df5
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-09-04 04:32:03 +0500

    design updates by abdullah

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-
-  constructor() { }
+totalAmount=0;
+  constructor(private api:ApiService) { }
 
   ngOnInit() {
+    this.total();
+  }
+  total(){
+let all=0;
+this.api.order.cart.forEach(elem=>{
+ all= elem.generalTotal+all;
+})
+this.totalAmount=all;
   }
 
 }
