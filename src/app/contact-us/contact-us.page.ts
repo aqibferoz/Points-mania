577a67916e82f9ecc818ee994148e4952cd71963
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-09-07 23:55:52 +0500

    update

--- src/app/contact-us/contact-us.page.ts
+++ src/app/contact-us/contact-us.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
+import { HelperService } from '../services/helper/helper.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.page.scss'],
 })
 export class ContactUsPage implements OnInit {
-
-  constructor() { }
+type='other';
+orderNo;
+subject;
+message;
+email;
+username;
+  constructor(private api:ApiService,private helper:HelperService) { }
 
   ngOnInit() {
+    this.api.getUser(localStorage.getItem('userId')).subscribe((res:any)=>{
+this.email=res.email;
+this.username=res.username;
+    })
   }
+check(event){
+console.log(event.target.value);
+if(event.target.value=='other'){
+  this.type='other'
+}else if(event.target.value=='order'){
+  this.type='order'
+}
+}
+otherQuery(){
 
+  this.helper.presentLoading('Submitting your request');
+  let data={
+    name:this.username,
+    email:this.email,
+    subject:this.subject,
+    message:this.message,
+    type:"other"
+  }
+  console.log(data);
+  this.api.submitQuery(data).then(res=>{
+    this.helper.dismissLoad();
+this.helper.presentToast('Stay Calm ! Let us see what can we provide you against your request')
+  }).catch(err=>{
+    this.helper.dismissLoad();
+    this.helper.presentToast('Something Went wrong');
+  })
+}
+orderQuery(){
+  let data={
+    name:this.username,
+    email:this.email,
+    subject:this.subject,
+    message:this.message,
+    orderNo:this.orderNo,
+    type:"order"
+  }
+  console.log(data);
+  this.api.submitQuery(data).then(res=>{
+    this.helper.dismissLoad();
+this.helper.presentToast('Stay Calm ! Let us see what can we provide you against your request')
+  })
+}
 }
