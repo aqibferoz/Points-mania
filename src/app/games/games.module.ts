6e4aa4ccca8b6e668ce8f92cc991573904dfd13e
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-10-25 05:33:37 +0500

    update

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { GamesPage } from './games.page';
+import{FooterComponent} from '../footer/footer.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [GamesPage]
+  declarations: [GamesPage,FooterComponent]
 })
 export class GamesPageModule {}
